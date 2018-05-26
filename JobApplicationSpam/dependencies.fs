9d98294e01a7ca258ea14910cc464f51632ab7b7
reneederer <reneederer@arcor.de>
2018-05-26 21:56:25 +0200

    Create a top menu

--- JobApplicationSpam/dependencies.fs
+++ JobApplicationSpam/dependencies.fs
@@ -8,5 +8,11 @@ module Dependencies =
         inherit Resources.BaseResource("https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/",
             "js/bootstrap.min.js", "css/bootstrap.min.css")
 
+    [<Require(typeof<JQuery.Resources.JQuery>)>]
+    [<Sealed>]
+    type FontAwesome() =
+        inherit Resources.BaseResource("https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css")
+
     [<assembly: Require(typeof<TwitterBootstrap>)>]
+    [<assembly: Require(typeof<FontAwesome>)>]
     do ()
